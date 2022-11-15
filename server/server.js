87cac683838acbdc0d4c74baeb9438389b6f259d
hoganrsherrow <hogan.r.sherrow@vanderbilt.edu>
2022-11-15 16:02:09 -0600

    Added Apollo Server. Added typeDefs and Resolvers for Reaction, Thought, and User models

--- server/server.js
+++ server/server.js
@@ -1,14 +1,52 @@
+// import express server
 const express = require('express');
+
+// import Apollo Server
+const { ApolloServer } = require('apollo-server-express');
+
+// import typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
+
+// connect to database
 const db = require('./config/connection');
 
+// establish PORT
 const PORT = process.env.PORT || 3001;
+
+// create a new Apollo server and pass in schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-  });
-});
+// create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+
+  // integrate our Apollo server with the Express application as middleware
+  server.applyMiddleware({ app });
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}.`);
+
+      // log where we can go to test out GraphQL API
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  })
+
+};
+
+// Call the async function to start the server
+startApolloServer(typeDefs, resolvers);
+
+// db.once('open', () => {
+//   app.listen(PORT, () => {
+//     console.log(`API server running on port ${PORT}!`);
+//   });
+// });
